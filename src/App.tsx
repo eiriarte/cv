491e33482054ea76d2280738288b615c99411e90
Eduardo Iriarte Gahete <eiriarte@gmail.com>
2024-06-06 09:49:37 +0200

    Menú de idioma

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import "../node_modules/i18nresume/dist/style.css";
 import AppBar from "./AppBar";
 import resume from "./assets/resume.json";
 import I18NResume from "i18nresume";
+import { useState } from "react";
 
 function App() {
+  const [locale, setLocale] = useState("en");
+  const handleLocalChange = (value: string) => {
+    setLocale(value);
+  };
   return (
     <>
-      <AppBar />
+      <AppBar locale={locale} onLocaleChange={handleLocalChange} />
       <div>
-        <I18NResume data={resume} locale="es" />
+        <I18NResume data={resume} locale={locale} />
       </div>
     </>
   );
--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -1,11 +1,40 @@
 import EsFlag from "./assets/es.svg?react";
 import EnFlag from "./assets/uk.svg?react";
-import { AppBar, IconButton, SvgIcon, Toolbar, Tooltip, Typography } from "@mui/material";
+import {
+  AppBar,
+  IconButton,
+  Menu,
+  MenuItem,
+  SvgIcon,
+  Toolbar,
+  Tooltip,
+  Typography
+} from "@mui/material";
+import { ElementType, useState } from "react";
 
-function Appbar() {
-  const locale = "en";
-  const Flag = locale === "es" ? EsFlag : EnFlag;
+export type LocaleChangeCallback = (value: string) => void;
+
+function Appbar({
+  locale,
+  onLocaleChange
+}: {
+  locale: string;
+  onLocaleChange: LocaleChangeCallback;
+}) {
+  const Flag = (locale === "es" ? EsFlag : EnFlag) as ElementType;
   const title = locale === "es" ? "Desarrollador Full-stack" : "Full-stack developer";
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleMenuItemClick = (_event: React.MouseEvent<HTMLElement>, newValue: string) => {
+    setAnchorEl(null);
+    onLocaleChange(newValue);
+  };
   return (
     <>
       <AppBar>
@@ -14,10 +43,32 @@ function Appbar() {
             {title}
           </Typography>
           <Tooltip title="Cambiar idioma">
-            <IconButton color="inherit">
+            <IconButton color="inherit" onClick={handleMenuClick}>
               <SvgIcon component={Flag} viewBox="0 0 512 512" sx={{ borderRadius: "50%" }} />
             </IconButton>
           </Tooltip>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button"
+            }}
+          >
+            <MenuItem
+              selected={locale === "en"}
+              onClick={(event) => handleMenuItemClick(event, "en")}
+            >
+              {locale === "es" ? "Inglés" : "English"}
+            </MenuItem>
+            <MenuItem
+              selected={locale === "es"}
+              onClick={(event) => handleMenuItemClick(event, "es")}
+            >
+              {locale === "es" ? "Español" : "Spanish"}
+            </MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
       <Toolbar variant="dense" />
